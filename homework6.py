dbe45c264200900d67e5a0a52889ecb5fb0a0886
rintrash <90425158+rintrash@users.noreply.github.com>
2022-02-27 14:29:31 -0800

    Added  readFile method

--- homework6.py
+++ homework6.py
@@ -10,9 +10,16 @@ class TwitterPositive():
         # input: any tweet text
         # output: a score [0,1], 0 means it is low quality and negative, 1 means it is high quality and positive
 
+        neg_words = readFile("negative-words.txt")
+        pos_words = readfile("positive-words.txt")
         return 0.5
         pass
 
+    def readFile(self):
+        file = open(fileName, "r")  
+        words = fileObj.read().splitlines()  # puts file into array
+        file.close()
+        return words
 
 # this is for testing only
 obj = TwitterPositive()
