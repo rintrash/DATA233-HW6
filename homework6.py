4902ee6310358713caeb64a916bde9fce19387a4
rintrash <90425158+rintrash@users.noreply.github.com>
2022-02-27 15:00:25 -0800

    Update homework6.py
    added situation where all positive

--- homework6.py
+++ homework6.py
@@ -1,43 +1,48 @@
-
-
 class TwitterPositive():
     def __init__(self):
         # do some initialization, optional
         pass
 
+    def readFile(self, fileName):
+        file = open(fileName, "r")
+        words = file.read().splitlines()  # puts file into array
+        file.close()
+        return words
+
     def evaluateTweet(self, tweet):
         # please implement this function
         # input: any tweet text
         # output: a score [0,1], 0 means it is low quality and negative, 1 means it is high quality and positive
-
-        neg_words = readFile("negative-words.txt")
-        pos_words = readfile("positive-words.txt")
+        tweet = tweet.split()
+        neg_words = self.readFile("negative-words.txt")
+        pos_words = self.readFile("positive-words.txt")
+        emphasis = ["very", "extremely", "super", "absolutely", "positively", "completely", "certainly"]
+        #we could add best, worst, fastest
         score = 0.5
-        
-        for word in tweet: 
-            
-            if word in pos_words: 
-                score += 0.05
-            if word in neg_words:
-                score -= 0.05
-            
-        if score > 1.0:
-            score = 1.0
-        if score < 0.0:
-            score = 0.0
-        
-        return score 
+        pos = 0
+        neg = 0
+        prev = tweet[0]
 
-    def readFile(self):
-        file = open(fileName, "r")  
-        words = fileObj.read().splitlines()  # puts file into array
-        file.close()
-        return words
+        for i in range(1, len(tweet)):
+            if tweet[i] in pos_words:
+                score += 0.05
+                pos += 1
+            if tweet[i] in neg_words:
+                score -= 0.05
+                neg += 1
+
+        if score > 1.0 or neg == 0:
+            score = 1.0
+        if score < 0.0 or pos == 0:
+            score = 0.0
+
+        return score
 
 # this is for testing only
 obj = TwitterPositive()
-if obj.evaluateTweet("DATA 233 is a wonderful class!") >= 0.5:
+score = obj.evaluateTweet("DATA 233 is a wonderful class!")
+if score >= 0.5:
+    print(score)
     print("Great, it is positive")
 else:
     print("negative")
-    
