b8fe92e30632774eab26ecfb68b3d98543f90e81
TMcDaneldJr <100326165+TMcDaneldJr@users.noreply.github.com>
2022-03-06 15:32:32 -0800

    Update sentenceQuality.py

--- sentenceQuality.py
+++ sentenceQuality.py
@@ -3,39 +3,86 @@
 # name 3: Travis McDaneld
 
 
-class sentenceQuality():
-    def __init__(self):
-        # do some initialization, optional
-        pass
+from textblob import TextBlob as tb
 
-    def calculateScores(self, tweet):
-        # please implement this function
-        # input: any tweet text
-        # output: a list of scores for the tweet, it must include: score for length, score for Polarity, score for Subjectivity, and at least one score of the following:
-        # https://en.wikipedia.org/wiki/Automated_readability_index
-        # https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
-        # https://en.wikipedia.org/wiki/Gunning_fog_index
-        # https://en.wikipedia.org/wiki/SMOG
-        # https://en.wikipedia.org/wiki/Fry_readability_formula
-        # https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
-        # You should implement at least one score
+class textScores():
+    
+    def __init__(self, text):
+        
+        self.b = tb(text)
+        
+        self.polarity = self.getPolarity()
+        self.subjectivity = self.getSubjectivity()
+#         self.length = self.getLength()
+#         self.repetitiveness = self.getRepetitiveness()
+        self.printResult()
+        
+    def printResult(self):
+        
+        print("Polarity Score: " + str(self.polarity) + "\n")
+        print("Subjectivity Score: " + str(self.subjectivity) + "\n")
+#         print("Length Score: " + self.length + "\n")
+#         print("Repetitiveness Score (High is Bad): " + self.repetitiveness)
+    
+    def getPolarity(self):
+        
+        self.pol = self.b.sentiment.polarity
+        if self.pol < 0:
+            true_pol = 0.5 - (abs(self.pol)/2)
+        elif self.pol > 0:
+            true_pol = 0.5 + (self.pol/2)
+        else:
+            true_pol = 0.5
+        return true_pol
+    
+    def getSubjectivity(self):
+        
+        self.sub = self.b.subjectivity
+        return self.sub
+    
+    def getLength(self):
+        # DO MORE STUFF HERE
+        if len(b) > 280:
+            self.length = 280
+            
 
-        return [0.1, 0.2, 0.3, 0.5, 0.6]
-        pass
-
-    def calculateQuality(self, scores):
-        # please implement this function to calculate a final quality score between 0 and 1
-        # Input: a list of scores, which is the output of calculateScores
-        # output: 0 means low quality, 1 mean high quality
-
-        return 0.5
-        pass
 
 
-# this is for testing only
-obj = sentenceQuality()
-s = "DATA 233 is a wonderful class!"
 
-print("The scores for your input is " + str(obj.calculateScores(s)))
 
-print("The final quality for your input is " + str(obj.calculateQuality(obj.calculateScores(s))))
+# class sentenceQuality():
+#     def __init__(self):
+#         # do some initialization, optional
+#         pass
+
+#     def calculateScores(self, tweet):
+#         # please implement this function
+#         # input: any tweet text
+#         # output: a list of scores for the tweet, it must include: score for length, score for Polarity, score for Subjectivity, and at least one score of the following:
+#         # https://en.wikipedia.org/wiki/Automated_readability_index
+#         # https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
+#         # https://en.wikipedia.org/wiki/Gunning_fog_index
+#         # https://en.wikipedia.org/wiki/SMOG
+#         # https://en.wikipedia.org/wiki/Fry_readability_formula
+#         # https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
+#         # You should implement at least one score
+
+#         return [0.1, 0.2, 0.3, 0.5, 0.6]
+#         pass
+
+#     def calculateQuality(self, scores):
+#         # please implement this function to calculate a final quality score between 0 and 1
+#         # Input: a list of scores, which is the output of calculateScores
+#         # output: 0 means low quality, 1 mean high quality
+
+#         return 0.5
+#         pass
+
+
+# # this is for testing only
+# obj = sentenceQuality()
+# s = "DATA 233 is a wonderful class!"
+
+# print("The scores for your input is " + str(obj.calculateScores(s)))
+
+# print("The final quality for your input is " + str(obj.calculateQuality(obj.calculateScores(s))))
