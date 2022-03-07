38923e5e4daaa28c021067fdf5b2ee927c838773
TMcDaneldJr <100326165+TMcDaneldJr@users.noreply.github.com>
2022-03-06 16:43:15 -0800

    Update sentenceQuality.py

--- sentenceQuality.py
+++ sentenceQuality.py
@@ -5,55 +5,55 @@
 
 from textblob import TextBlob as tb
 
-
 class sentenceQuality():
-
-    def __init__(self):
-        pass
-
+        
     def calculateScores(self, text):
-        # please implement this function
-        # input: any tweet text
-        # output: a list of scores for the tweet, it must include: score for length, score for Polarity,
-        # score for Subjectivity, and at least readability score
+        
         b = tb(text)
-
-        #length
-        words = b.split()
-        length_score = 0.0
-        if len(words) > 20:
-            length_score = 1.0
-        else:
-            length_score = len(words) / 20
-
-        #polarity
-        pol = b.sentiment.polarity
+        
+        polarity = self.getPolarity(b)
+        subjectivity = self.getSubjectivity(b)
+        length = self.getLength(b)
+        readability = self.getReadability(b)
+        
+        results = [length, polarity, subjectivity, readability]
+        return results
+    
+    def calculateQuality(self, text):
+        
+        return sum(text)/4
+        
+    def getPolarity(self, text):
+        
+        pol = text.sentiment.polarity
         if pol < 0:
-            true_pol = 0.5 - (abs(pol) / 2)
+            true_pol = 0.5 - (abs(pol)/2)
         elif pol > 0:
-            true_pol = 0.5 + (pol / 2)
+            true_pol = 0.5 + (pol/2)
         else:
             true_pol = 0.5
-
-        #subjectivity
-        subjectivity = b.subjectivity
-
-        #readability
-        lines = b.split(".")
-        words = b.split(' ')
-        characters = len(b.replace(".", " "))
-        ARIvalue = 4.71 * (characters / len(words)) + (0.5 * (len(words) / len(lines))) - 21.43
-        readability_score = (round(ARIvalue) - 1) / 13
-
-        return [length_score, true_pol, subjectivity,
-                readability_score]
-
-    def calculateQuality(self, text):
-        # please implement this function to calculate a final quality score between 0 and 1
-        # Input: a list of scores, which is the output of calculateScores
-        # output: 0 means low quality, 1 mean high quality
-
-        return sum(self.calculateScores(text)) / 4
+        return true_pol
+    
+    def getSubjectivity(self, text):
+        
+        sub = text.subjectivity
+        return sub
+    
+    def getLength(self, text):
+        words = text.split()
+        if len(words) > 20:
+            return 1.0
+        else:
+            return len(words) / 20
+        
+        
+    def getReadability(self, text):
+        lines = text.split(".")
+        words = text.split(' ')
+        characters = len(text.replace("."," "))
+        ARIvalue = 4.71*(characters/len(words)) + (0.5*(len(words)/len(lines))) - 21.43
+        value = round(ARIvalue) - 1
+        return value/13
 
 
 # # this is for testing only
