9749b079995edf5dff118d28fadf8f481b3292c9
Anyah Arboine <91972943+aarboine@users.noreply.github.com>
2022-03-14 20:14:48 -0700

    Update Data233_HW9.py

--- Data233_HW9.py
+++ Data233_HW9.py
@@ -235,5 +235,27 @@ assert precision(70, 4930, 13930, 981070) == 0.014
 def recall(tp: int, fp: int, fn: int, tn: int) -> float:
     
     pass
+
+
+
+
+def accuracy(tp: int, fp: int, fn: int, tn: int) -> float:
+    correct = tp + tn
+    total = tp + fp + fn + tn
+    return correct / total
+
+assert accuracy(70, 4930, 13930, 981070) == 0.98114
+
+def precision(tp: int, fp: int, fn: int, tn: int) -> float:
+    
+    return tp / (tp + fp)
+
+assert precision(70, 4930, 13930, 981070) == 0.014
+
+def recall(tp: int, fp: int, fn: int, tn: int) -> float:
+    
+    return tp / (tp + fn)
+           
+assert recall(70, 4930, 13930, 981070) == 0.005
            
 assert recall(70, 4930, 13930, 981070) == 0.005
